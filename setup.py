a825cd68a684214cb0a2e9298c05906e3ad7db5e
semiexp <linthya.v@gmail.com>
2025-02-24 13:44:20 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup, find_packages
+import os
+import re
+from setuptools import setup
+
+
+SHARED_LIB = re.compile(r"pycsugar\..*\.(so|dylib|dll|pyd)")
+
+
+build_dir = "build"
+package_data = []
+for file in os.listdir(build_dir):
+    if SHARED_LIB.match(file):
+        package_data.append(file)
+
+assert len(package_data) == 1
 
 
 setup(
-    name='pycsugar',
-    version='0.0.1',
-    packages=[''],
-    package_dir={'': 'build'},
-    package_data={'': ['pycsugar.cpython-38-x86_64-linux-gnu.so']}
+    name="pycsugar",
+    version="0.0.1",
+    packages=[""],
+    package_dir={"": build_dir},
+    package_data={"": package_data},
 )
